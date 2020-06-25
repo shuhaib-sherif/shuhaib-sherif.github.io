3809fc8204bce071a7485917d477e8d90912dadd
shuhaib sherif <shuhaibsherif@gmail.com>
2020-06-25 10:02:04 +0530

    lol

---
+++ keeper/build/precache-manifest.356dfc1b4047f959b63e2c6c04c824ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d01c25cfd8da9b8b3f4188c78c090d",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "fb7e87154f96651a0a53",
+    "url": "/keeper/static/js/2.9c3d3d40.chunk.js"
+  },
+  {
+    "revision": "575698c64289243906e9",
+    "url": "/keeper/static/js/main.c6b53ee1.chunk.js"
+  },
+  {
+    "revision": "41cf8d275794691b25e2",
+    "url": "/keeper/static/js/runtime-main.d6409cd2.js"
+  }
+]);
